58cc21e4dd722e8862c4f96141133b5d91e7d8cf
marvinoeben <oebenmarvin@gmail.com>
2016-06-25 17:42:59 +0200

    First working mask projection

--- mask/mask.ino
+++ mask/mask.ino
@@ -1,7 +1,12 @@
-#include "FastLED.h"
+// Include FastLED library and other useful files
+#include <FastLED.h>
+#include <EEPROM.h>
 #include "XYmap.h"
-#define DATA_PIN 14
+#include "utils.h"
+#include "effects.h"
 
+#define DATA_PIN 14
+#define CLOCK_PIN    5
 // Number of leds defined in XYmap.h
 
 void setup() { 
@@ -10,31 +15,16 @@ void setup() {
       FastLED.setBrightness(50);
 }
 
-void plasma() {
-
-  static byte offset  = 0; // counter for radial color wave motion
-  static int plasVector = 0; // counter for orbiting plasma center
-
-  // Calculate current center of plasma pattern (can be offscreen)
-  int xOffset = cos8(plasVector / 256);
-  int yOffset = sin8(plasVector / 256);
-
-  // Draw one frame of the animation into the LED array
-  for (int x = 0; x < kMatrixWidth; x++) {
-    for (int y = 0; y < kMatrixHeight; y++) {
-      byte color = sin8(sqrt(sq(((float)x - 7.5) * 10 + xOffset - 127) + sq(((float)y - 2) * 10 + yOffset - 127)) + offset);
-      leds[XY(x, y)] = CHSV(color, 255, 255);
-    }
-  }
-
-  offset++; // wraps at 255 for sin8
-  plasVector += 16; // using an int for slower orbit (wraps at 65536)
-
-}
-
-
 void loop() {
-  plasma();
-  FastLED.show(); 
-}
+//  plasma();
+//  threeSine();
+//  threeDee();
+//  confetti();
+//  rider();
+//  glitter();
+  slantBars();
+//  colorFill();
+//  sideRain();
+  FastLED.show();
+ }
 
