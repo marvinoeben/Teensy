861167882eefb2de51071e915158cdd7c6f8c272
marvinoeben <oebenmarvin@gmail.com>
2016-06-25 19:08:18 +0200

    Some small adjustments to the file (not in every commit, advised is a local 'play' file in a .gitignore

--- mask/mask.ino
+++ mask/mask.ino
@@ -14,17 +14,42 @@ void setup() {
       FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
       FastLED.setBrightness(50);
 }
+functionList effectList[] = {single,
+                           /*  threeSine,
+                             plasma,
+                             confetti,
+                             glitter,
+                             slantBars,
+                             colorFill,
+                             sideRain */
+                            };
+const byte numEffects = (sizeof(effectList)/sizeof(effectList[0]));
 
 void loop() {
-//  plasma();
-//  threeSine();
-//  threeDee();
-//  confetti();
-//  rider();
-//  glitter();
-  slantBars();
-//  colorFill();
-//  sideRain();
-  FastLED.show();
+  currentMillis = millis(); // save the current timer value
+  // switch to a new effect every cycleTime milliseconds
+  if (currentMillis - cycleMillis > cycleTime && autoCycle == true) {
+    cycleMillis = currentMillis;
+    if (++currentEffect >= numEffects) currentEffect = 0; // loop to start of effect list
+    effectInit = false; // trigger effect initialization when new effect is selected
+  }
+
+  // increment the global hue value every hueTime milliseconds
+  if (currentMillis - hueMillis > hueTime) {
+    hueMillis = currentMillis;
+    hueCycle(1); // increment the global hue value
+  }
+
+  // run the currently selected effect every effectDelay milliseconds
+  if (currentMillis - effectMillis > effectDelay) {
+    effectMillis = currentMillis;
+    effectList[currentEffect](); // run the selected effect function
+    random16_add_entropy(1); // make the random values a bit more random-ish
+  }
+
+  // run a fade effect too if the confetti effect is running
+  if (effectList[currentEffect] == confetti) fadeAll(1);
+
+  FastLED.show(); // send the contents of the led memory to the LEDs
  }
 
