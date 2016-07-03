830e40b4a464a9041deed6574eefa6796fba90d1
Neppiez <neppiez@gmail.com>
2016-07-03 14:49:41 +0200

    fixed radar/put in seperate file

--- mask/mask.ino
+++ mask/mask.ino
@@ -4,19 +4,19 @@
 #include "XYmap.h"
 #include "utils.h"
 #include "effects.h"
-#include "vrain.h"
+#include "radar.h"
 
 #define DATA_PIN 14
 
 // Number of leds defined in XYmap.h
 
-void setup() {
+void setup() { 
       // Uncomment/edit one of the following lines for your leds arrangement.
       FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
       FastLED.setBrightness(50); // MN: Can't change this value on the fly so need to be included as Hue value in every function.
-
+	  
 	  pinMode(13, OUTPUT);
-
+	  
 }
 
 #include "runfile.h" #kleine comment
--- mask/radar.h
+++ mask/radar.h
@@ -1,25 +1,24 @@
+int radar_n = 5; // Number of colors
+//  int speed = 5;
+int radar_x = 0;
+int radar_y = 0;
+const float radar_pi = 2 *  3.1415;
+float radar_angle = 0;
 void radar() {
   fadeAll(75);
-  int n = 5; // Number of colors
-  int speed = 5;
-  int x = 0;
-  int y = 0;
-  const pi = 3.1415;
-  float radar_angle;
-  EVERY_N_MILLISECONDS(speed) {
-  for (int k = 0; k < n; k++ ){
-    x = 0;
-    y = 0;
-    radar_angle = k * pi / n;
-    int i = 0;
-    while (i < 11) {
-      x = cos(radar_angle)*i;
-      y = sin(radar_angle)*i;
-      leds[XY((kMatrixWidth/2)+x, (kMatrixHeight/2)+y)] = CHSV(round(k * 255 / n), 255, 255);
-      i++;
+  EVERY_N_MILLISECONDS(10) {
+    for (int k = 0; k < radar_n; k++ ){
+      radar_x = 0;
+      radar_y = 0;
+      float radar_angle2 = radar_angle + k * radar_pi / radar_n;
+      int i = 0;
+      while (i < 11) {
+        radar_x = cos(radar_angle2)*i;
+        radar_y = sin(radar_angle2)*i;
+        leds[XY((kMatrixWidth/2)+radar_x, (kMatrixHeight/2)+radar_y)] = CHSV(round(k * 255 / radar_n), 255, 255);
+        i++;
+      }
     }
-    radar_angle+=(3.141596/100);
-  }
-
+  radar_angle += radar_pi/100;
   }
 }
